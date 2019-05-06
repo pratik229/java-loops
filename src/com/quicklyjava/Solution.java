ff5d26ca2dfe0e0a95f6dcc1e84abe175aa7ac1e
Pratik Patel <pratik229@gmail.com>
2019-05-06 08:41:32 -0400

    Documenting.

--- src/com/quicklyjava/Solution.java
+++ src/com/quicklyjava/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        
+        //loop
         for(int i=1; i<=10; i++){
         	System.out.println(n+" x " +i+ " = "+n*i);
         }
